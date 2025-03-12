34f29970f71765c12d6b73152054eeb4d9d5aa8f
FomenkoDmytro <147436958+FomenkoDmytro@users.noreply.github.com>
2025-03-12 21:19:33 +0200

    Reinitialize repo with proper .gitignore

---
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using EpicVision.Application.Interfaces;
+using EpicVision.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        // Получить все фильмы
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_movieService.GetAllMovies());
+
+        // Получить фильм по ID
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var movie = _movieService.GetMovieById(id);
+            return movie != null ? Ok(movie) : NotFound();
+        }
+
+        // Создать новый фильм
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            _movieService.AddMovie(movie);
+            return CreatedAtAction(nameof(Get), new { id = movie.Id }, movie);
+        }
+
+        // Обновить фильм
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Movie movie)
+        {
+            if (id != movie.Id) return BadRequest();
+            _movieService.UpdateMovie(movie);
+            return NoContent();
+        }
+
+        // Удалить фильм
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _movieService.DeleteMovie(id);
+            return NoContent();
+        }
+    }
+}
---
+++ CinemaAPI/Program.cs
@@ -0,0 +1,49 @@
+using EpicVision.Application.Interfaces;
+using EpicVision.Application.Services;
+using EpicVision.Infrastructure.Data;
+using EpicVision.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using System.Threading;
+
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+// Add services to the container.
+builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+builder.Services.AddScoped<IMovieService, MovieService>();
+
+
+builder.Services.AddControllers();
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
