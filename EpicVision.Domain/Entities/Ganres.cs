c8281cdef348300681db76c6cbe0ac098b179b59
FomenkoDmytro <147436958+FomenkoDmytro@users.noreply.github.com>
2025-03-17 19:47:43 +0200

    added ganres table

---
+++ EpicVision.Domain/Entities/Ganres.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EpicVision.Domain.Entities
+{
+    internal class Ganres
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- EpicVision.Domain/Entities/Movie.cs
+++ EpicVision.Domain/Entities/Movie.cs
@@ -1,19 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EpicVision.Domain.Entities
 {
     public class Movie
     {
         [Key]
+        [Column("movie_id")]
         public int Id { get; set; }
 
         [Required]
+        [Column("movie_title")]
         public string Title { get; set; } = string.Empty;
 
         [Required]
+        [Column("movie_genre")]
         public string Genre { get; set; } = string.Empty;
 
         [Required]
+        [Column("movie_duration")]
         public int DurationInMinutes { get; set; }
+
+        [Required]
+        [Column("movie_description")]
+        public string Description { get; set; } = string.Empty;
     }
 }
